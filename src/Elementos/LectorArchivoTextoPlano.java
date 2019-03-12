e9239fabad1068e162fdb0f928406b6d39d313f2
Daniel Gutierrez <danielgudu22@gmail.com>
2019-03-12 16:27:01 -0500

    Lector de Archivos de textos.

--- src/Elementos/LectorArchivoTextoPlano.java
+++ src/Elementos/LectorArchivoTextoPlano.java
@@ -5,10 +5,38 @@
  */
 package Elementos;
 
+import Exceptions.TextoVacioException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.swing.JOptionPane;
 /**
  *
  * @author USUARIO
  */
 public class LectorArchivoTextoPlano {
     
+    private String texto = "src/Archivos/EditarVehiculo.txt";
+    
+    public void leerTexto() throws Exception {
+        
+            File file = new File(texto);
+            FileReader fileR = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fileR);
+        
+            String text = "";
+        
+        try {
+
+            while ((text = reader.readLine()) != null) {
+                System.out.println(text);
+            }
+
+        } catch (IOException e) {
+            if(file.length() == 0) {
+                throw new TextoVacioException("No se ha ingresado una especificación correcta.");
+            }
+        }
+    }
 }
--- src/Exceptions/TextoVacioException.java
+++ src/Exceptions/TextoVacioException.java
@@ -3,16 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Elementos;
+package Exceptions;
 
 /**
  *
  * @author USUARIO
  */
-public class Simulador {
-    public static void main(String[] args) throws Exception {
-        LectorArchivoTextoPlano lector = new LectorArchivoTextoPlano();
-        lector.leerTexto();
+public class TextoVacioException extends Exception {
+
+    public TextoVacioException(String message) {
+        super(message);
     }
     
 }
