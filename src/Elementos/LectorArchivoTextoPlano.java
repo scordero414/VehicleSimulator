2cb74780c0577fdcb116a876bb16b68da108975f
Daniel Gutierrez <danielgudu22@gmail.com>
2019-03-12 17:19:14 -0500

    Modificación cargar archivo de texto.

--- src/Elementos/LectorArchivoTextoPlano.java
+++ src/Elementos/LectorArchivoTextoPlano.java
@@ -18,20 +18,26 @@ import javax.swing.JOptionPane;
 public class LectorArchivoTextoPlano {
     
     private String texto = "src/Archivos/EditarVehiculo.txt";
-    
+    private String tipoLlanta;
+    private String cilindrajeMotor;
+        
     public void leerTexto() throws Exception {
-        
-            File file = new File(texto);
-            FileReader fileR = new FileReader(file);
-            BufferedReader reader = new BufferedReader(fileR);
-        
-            String text = "";
+            
+        String arr[];
+        File file = new File(texto);
         
         try {
-
-            while ((text = reader.readLine()) != null) {
-                System.out.println(text);
+            FileReader fileR = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fileR);
+            
+            String text = "";
+            String line = reader.readLine();
+            while (line != null) {
+                text += line;
+                line = reader.readLine();
             }
+            arr = text.split(" ");
+            System.out.println(arr[1] + " " + arr[3]);
 
         } catch (IOException e) {
             if(file.length() == 0) {
--- src/Elementos/Simulador.java
+++ src/Elementos/Simulador.java
@@ -14,6 +14,7 @@ import vistas.VistaVehiculo;
 public class Simulador {
     public static void main(String[] args) throws Exception {
         LectorArchivoTextoPlano lector = new LectorArchivoTextoPlano();
+        lector.leerTexto();
     }
     
 }
